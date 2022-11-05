e08cfe1996b6b388865c7e3d5ef8c48c6c9a63b5
theartcher <jorisplayz@gmail.com>
2022-11-05 02:44:02 +0100

    Added basic skeleton of registry screen, removed incompatible components
    Removed 'body' and 'div' turned out to be invalid objects. Added a basic skeleton, button and navigation to a registry screen. Added basic styling.

--- App.js
+++ App.js
@@ -1,30 +1,45 @@
-
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Div>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
           onPress={() => navigation.navigate('Details')}
         />
-      </Div>
+
+      <Button
+          title="Register"
+          onPress={() => navigation.navigate('Register')}
+        />
     </View>
   );
 }
 
 function DetailsScreen() {
   return (
-    <Body className="body">
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
       </View>
-    </Body>
+  );
+}
+
+function RegisterScreen() {
+  return (
+      <View>
+        <Text>Trash 4 Cash</Text>
+        <Text>Account aanmaken</Text>
+
+        <TextInput
+          style={styles.registerScreen}
+          placeholder="Voornaam"
+          keyboardType='default'
+        />
+      </View>
   );
 }
 
@@ -37,10 +52,20 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  registerScreen: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default App;
 
