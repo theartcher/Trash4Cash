890344dfc5d28b2afd039ff4d0cb8a18ccfee0d6
theartcher <jorisplayz@gmail.com>
2022-11-05 16:15:01 +0100

    Fixed formatting, added styling and started register page

--- App.js
+++ App.js
@@ -2,51 +2,52 @@ import * as React from 'react';
 import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-        />
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
 
       <Button
-          title="Register"
-          onPress={() => navigation.navigate('Register')}
-        />
+        title="Register"
+        onPress={() => navigation.navigate('Register')}
+      />
     </View>
   );
 }
 
 function DetailsScreen() {
   return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
   );
 }
 
 function RegisterScreen() {
   return (
-      <View>
-        <Text>Trash 4 Cash</Text>
+    <View style={styles.container}>
+      <View style={styles.registerScreenMainBlock}>
+        <Text style={styles.title}>Trash 4 Cash</Text>
         <Text>Account aanmaken</Text>
-
-        <TextInput
-          style={styles.registerScreen}
-          placeholder="Voornaam"
-          keyboardType='default'
-        />
+        <TextInput placeholder="Voornaam" keyboardType="default" />
       </View>
+    </View>
   );
 }
-
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [fontsLoaded] = useFonts({
+    Minecraft: require('./assets/fonts/Minecraft.ttf'),
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -59,13 +60,39 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-  registerScreen: {
-    height: 40,
+  //VIEW
+  registerScreenMainBlock: {
     margin: 12,
-    borderWidth: 1,
     padding: 10,
+    borderRadius: 5,
+    width: '98%',
+    height: '98%',
+    backgroundColor: '#2C2F33',
+  },
+  //VIEW
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#7289DA',
+    padding: 20,
+    margin: 10,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  //TEXT
+  title: {
+    color: 'white',
+    padding: 10,
+    fontSize: 40,
+    textShadowColor: 'black',
+    fontFamily: 'Minecraft',
+  },
+  defaultTextFormatting: {
+    fontFamily: 'Minecraft',
+    color: 'white',
+    fontSize: 20,
   },
 });
 
 export default App;
-
