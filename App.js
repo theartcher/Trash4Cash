3c19983f878393686ed7ace09b9d3ce56db97600
theartcher <jorisplayz@gmail.com>
2022-11-06 23:46:43 +0100

    Added colour variables, expanded registry page, added custom button, fixed some formatting issues, added dictionary for project (Code Spell Checker)

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+
+const accentColour = '#00A896';
+const backgroundColour = '#2C2F33';
+
+/*
+TODO: Separate styling to it's own CSS file
+TODO: Make custom buttons
+TODO: Better names for styling and more centralized colour palettes
+! REMOVE INLINE STYLING
+*/
 
 function HomeScreen({ navigation }) {
   return (
@@ -29,13 +46,46 @@ function DetailsScreen() {
   );
 }
 
-function RegisterScreen() {
+function RegistryScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.registerScreenMainBlock}>
-        <Text style={styles.title}>Trash 4 Cash</Text>
-        <Text>Account aanmaken</Text>
-        <TextInput placeholder="Voornaam" keyboardType="default" />
+      <View style={styles.mainContainerBlock}>
+        <Text style={styles.titleText}>Trash 4 Cash</Text>
+        <Text style={styles.defaultText}>Account aanmaken</Text>
+        <TextInput
+          style={styles.userTextInput}
+          placeholder="Email"
+          placeholderTextColor={accentColour}
+          color={accentColour}
+        />
+
+        <TextInput
+          style={styles.userTextInput}
+          placeholder="Name"
+          placeholderTextColor={accentColour}
+          color={accentColour}
+        />
+
+        <TextInput
+          style={styles.userTextInput}
+          placeholder="Username"
+          placeholderTextColor={accentColour}
+          color={accentColour}
+        />
+
+        <TextInput
+          style={styles.userTextInput}
+          placeholder="Password"
+          placeholderTextColor={accentColour}
+          color={accentColour}
+        />
+
+        <Pressable
+          style={styles.pressableButton}
+          onPress={() => alert('User account successfully registered.')}
+        >
+          <Text>Create account</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -46,6 +96,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   const [fontsLoaded] = useFonts({
     Minecraft: require('./assets/fonts/Minecraft.ttf'),
+    Montserrat: require('./assets/fonts/Montserrat-Medium.ttf'),
   });
 
   return (
@@ -53,7 +104,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Register" component={RegistryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -61,37 +112,56 @@ function App() {
 
 const styles = StyleSheet.create({
   //VIEW
-  registerScreenMainBlock: {
-    margin: 12,
+  mainContainerBlock: {
     padding: 10,
     borderRadius: 5,
-    width: '98%',
+    width: '90%',
     height: '98%',
-    backgroundColor: '#2C2F33',
+    backgroundColor: backgroundColour,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   //VIEW
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    backgroundColor: '#7289DA',
-    padding: 20,
-    margin: 10,
+    padding: 5,
+    backgroundColor: accentColour,
     alignItems: 'center',
     flexDirection: 'column',
     justifyContent: 'center',
   },
   //TEXT
-  title: {
+  titleText: {
     color: 'white',
     padding: 10,
     fontSize: 40,
-    textShadowColor: 'black',
-    fontFamily: 'Minecraft',
+    fontFamily: 'Monserrat',
+    fontWeight: 'bold',
   },
-  defaultTextFormatting: {
-    fontFamily: 'Minecraft',
+  defaultText: {
+    fontFamily: 'Monserrat',
     color: 'white',
     fontSize: 20,
+    textAlign: 'left',
+  },
+  //TextInput
+  userTextInput: {
+    padding: 5,
+    margin: 10,
+    borderColor: 'white',
+    borderWidth: 0.75,
+    borderRadius: 5,
+    height: '10%',
+    width: '80%',
+    fontStyle: 'italic',
+    textColor: 'white',
+  },
+  pressableButton: {
+    backgroundColor: accentColour,
+    borderWidth: 0,
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
   },
 });
 
